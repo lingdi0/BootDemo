951c1e35beeda71ad62c62b2303c05810ab6a34e
lj <leijievip0@163.com>
2019-11-01 18:40:41 +0800

    时间段Util和验证Util

--- src/main/java/common/util/TimeQuantumUtil.java
+++ src/main/java/common/util/TimeQuantumUtil.java
@@ -26,11 +26,9 @@ public class TimeQuantumUtil {
 	 * @return
 	 */
 	public static boolean isBetween(LocalDateTime time,TimeQuantum timeQuantum) {
-		if(timeQuantum.getStart().isAfter(timeQuantum.getEnd())) {
-			throw new IllegalArgumentException("开始时间大于结束时间");
-		}
+		checkTimeQuantum(timeQuantum);
 		long minutes;
-		return (minutes=ChronoUnit.MINUTES.between(timeQuantum.getStart(), time))>0?minutes<=ChronoUnit.MINUTES.between(timeQuantum.getStart(), timeQuantum.getEnd()):false;
+		return (minutes=ChronoUnit.MINUTES.between(timeQuantum.getStart(), time))>=0?minutes<=ChronoUnit.MINUTES.between(timeQuantum.getStart(), timeQuantum.getEnd()):false;
 	}
 	/**
 	 * 合并时间段
@@ -42,10 +40,8 @@ public class TimeQuantumUtil {
 		if(timeQuantums==null||timeQuantums.size()==0) {
 			return Collections.EMPTY_LIST;
 		}
-		ArrayList<TimeQuantum> list = new ArrayList<TimeQuantum>();
-		for (TimeQuantum timeQuantum : timeQuantums) {
-			list.add(new TimeQuantum().setStart(timeQuantum.getStart()).setEnd(timeQuantum.getEnd()));
-		}
+		ArrayList<TimeQuantum> list = new ArrayList<TimeQuantum>(timeQuantums);
+		checkTimeQuantums(list);
 		int startCnt = 0;
 		int endCnt = -1;
 		while(startCnt!=endCnt) {
@@ -53,10 +49,6 @@ public class TimeQuantumUtil {
 			for(int i = list.size()-1;i>=0;i--) {
 				TimeQuantum source = list.get(i);
 				for(int j = i-1;j>=0;j--) {
-					if(source.getStart().isAfter(source.getEnd())) {
-						list.remove(i);
-						break;
-					}
 					TimeQuantum target = list.get(j);
 					if(isCover(source, target)){
 						target.setStart(source.getStart().isBefore(target.getStart())?source.getStart():target.getStart());
@@ -76,28 +68,26 @@ public class TimeQuantumUtil {
 		if(totalBase==null||totalBase.size()==0) {
 			return Collections.EMPTY_LIST;
 		}
-		List<TimeQuantum> total = new ArrayList<TimeQuantum>();
-		for (TimeQuantum timeQuantum : totalBase) {
-			total.add(new TimeQuantum().setStart(timeQuantum.getStart()).setEnd(timeQuantum.getEnd()));
-		}
+		List<TimeQuantum> total = new ArrayList<TimeQuantum>(totalBase);
+		checkTimeQuantums(total);
+		total = merge(total);
 		if(subBase==null||subBase.size()==0) {
-			return merge(total);
+			return total;
 		}
-		List<TimeQuantum> sub = new ArrayList<TimeQuantum>();
-		for (TimeQuantum timeQuantum : subBase) {
-			sub.add(new TimeQuantum().setStart(timeQuantum.getStart()).setEnd(timeQuantum.getEnd()));
-		}
+		List<TimeQuantum> sub = new ArrayList<TimeQuantum>(subBase);
+		checkTimeQuantums(sub);
+		sub = merge(sub);
 		boolean change;
 		do {
 			change = false;
 			for(int i = total.size()-1;i>=0;i--) {
 				TimeQuantum source = total.get(i);
+				if(source.getStart().isAfter(source.getEnd())) {
+					total.remove(i);
+					change = true;
+					break;
+				}
 				for(int j = sub.size()-1;j>=0;j--) {
-					if(source.getStart().isAfter(source.getEnd())) {
-						total.remove(i);
-						change = true;
-						break;
-					}
 					TimeQuantum target = sub.get(j);
 					if(target.getStart().isAfter(target.getEnd())) {
 						sub.remove(j);
@@ -108,7 +98,7 @@ public class TimeQuantumUtil {
 						total.add(new TimeQuantum().setStart(target.getEnd()).setEnd(source.getEnd()));
 						source.setEnd(target.getStart());
 						change = true;
-					}else if(isBetween(target.getStart(), source)&&!target.getEnd().isBefore(source.getEnd())) {
+					}else if(isBetween(target.getStart(), source)&&!target.getEnd().isBefore(source.getEnd())&&!source.getEnd().equals(target.getStart())) {
 						source.setEnd(target.getStart());
 						change = true;
 					}else if(!target.getStart().isAfter(source.getStart())&&isBetween(target.getEnd(), source)&&!target.getEnd().equals(source.getStart())) {
@@ -133,26 +123,33 @@ public class TimeQuantumUtil {
 	}
 	
 	public static boolean isIndependent(TimeQuantum t1,TimeQuantum t2) {
-		Objects.requireNonNull(t1);
-		Objects.requireNonNull(t2);
-		if(t1.getEnd().isBefore(t1.getStart())||t2.getEnd().isBefore(t2.getStart())) {
-			throw new IllegalArgumentException("开始时间大于结束时间");
-		}
-		long minutes1 = ChronoUnit.MINUTES.between(t1.getStart(), t1.getEnd());
-		long minutes2 = ChronoUnit.MINUTES.between(t2.getStart(), t2.getEnd());
-		return ChronoUnit.MINUTES.between(t1.getStart(), t2.getStart())>minutes1||ChronoUnit.MINUTES.between(t2.getStart(), t1.getStart())>minutes2;
+		return !isCover(t1, t2);
 	}
 	public static boolean isCover(TimeQuantum t1,TimeQuantum t2) {
-		return !isIndependent(t1, t2);
+		Objects.requireNonNull(t1);
+		Objects.requireNonNull(t2);
+		checkTimeQuantum(t1);
+		checkTimeQuantum(t2);
+		return isBetween(t1.getStart(), t2)||isBetween(t1.getEnd(), t2)||isBetween(t2.getStart(), t1)||isBetween(t2.getEnd(), t1);
+	}
+	
+	private static void checkTimeQuantum(TimeQuantum t) {
+		if(t.getStart().isAfter(t.getEnd())) {
+			throw new IllegalArgumentException("开始时间大于结束时间");
+		}
+	}
+	
+	private static void checkTimeQuantums(List<TimeQuantum> ts) {
+		ts.forEach(TimeQuantumUtil::checkTimeQuantum);
 	}
 	
 	public static void main(String[] args) {
 		LocalDateTime d1 = LocalDateTime.parse("2019-08-27T12:00:00");
 		LocalDateTime d2 = LocalDateTime.parse("2019-08-27T15:00:00");
-		LocalDateTime d3 = LocalDateTime.parse("2019-08-27T15:30:00");
+		LocalDateTime d3 = LocalDateTime.parse("2019-08-27T14:30:00");
 		LocalDateTime d4 = LocalDateTime.parse("2019-08-27T17:00:00");
-		LocalDateTime d5 = LocalDateTime.parse("2019-08-27T11:00:00");
-		LocalDateTime d6 = LocalDateTime.parse("2019-08-27T15:00:00");
+		LocalDateTime d5 = LocalDateTime.parse("2019-08-27T14:00:00");
+		LocalDateTime d6 = LocalDateTime.parse("2019-08-27T19:00:00");
 		TimeQuantum t1 = new TimeQuantum().setStart(d1).setEnd(d2);
 		TimeQuantum t2 = new TimeQuantum().setStart(d3).setEnd(d4);
 		TimeQuantum t3 = new TimeQuantum().setStart(d5).setEnd(d6);
@@ -161,18 +158,19 @@ public class TimeQuantumUtil {
 		list.add(t1);
 		list.add(t2);
 		list2.add(t3);
-		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(2)), new TimeQuantum(d1.plusHours(3), d1.plusHours(4))));
-		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(2)), new TimeQuantum(d1.plusHours(2), d1.plusHours(4))));
-		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(2)), new TimeQuantum(d1.plusHours(1), d1.plusHours(4))));
-		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(2)), new TimeQuantum(d1.plusHours(0), d1.plusHours(4))));
-		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(2)), new TimeQuantum(d1.plusHours(-1), d1.plusHours(4))));
+//		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(2)), new TimeQuantum(d1.plusHours(3), d1.plusHours(4))));
+//		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(2)), new TimeQuantum(d1.plusHours(2), d1.plusHours(4))));
+//		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(2)), new TimeQuantum(d1.plusHours(1), d1.plusHours(4))));
+//		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(2)), new TimeQuantum(d1.plusHours(0), d1.plusHours(4))));
+//		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(2)), new TimeQuantum(d1.plusHours(-1), d1.plusHours(4))));
+//		
+//		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(3), d1.plusHours(4)), new TimeQuantum(d1.plusHours(0), d1.plusHours(2))));
+//		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(2), d1.plusHours(4)), new TimeQuantum(d1.plusHours(0), d1.plusHours(2))));
+//		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(1), d1.plusHours(4)), new TimeQuantum(d1.plusHours(0), d1.plusHours(2))));
+//		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(4)), new TimeQuantum(d1.plusHours(0), d1.plusHours(2))));
+//		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(-1), d1.plusHours(4)), new TimeQuantum(d1.plusHours(0), d1.plusHours(2))));
+		System.out.println(sub(list,list2));
 		
-		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(3), d1.plusHours(4)), new TimeQuantum(d1.plusHours(0), d1.plusHours(2))));
-		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(2), d1.plusHours(4)), new TimeQuantum(d1.plusHours(0), d1.plusHours(2))));
-		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(1), d1.plusHours(4)), new TimeQuantum(d1.plusHours(0), d1.plusHours(2))));
-		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(0), d1.plusHours(4)), new TimeQuantum(d1.plusHours(0), d1.plusHours(2))));
-		System.out.println(isIndependent(new TimeQuantum(d1.plusHours(-1), d1.plusHours(4)), new TimeQuantum(d1.plusHours(0), d1.plusHours(2))));
-				
 	}
 
 }
